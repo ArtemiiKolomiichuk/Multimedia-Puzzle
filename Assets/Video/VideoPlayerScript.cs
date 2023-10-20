0ad6c8e9be67b413cebbf3b5c3d3706f4fceb302
AMykolaD <andrusenkomykola@gmail.com>
2023-10-20 12:40:44 +0300

    Fixed video

--- Assets/Video/VideoPlayerScript.cs
+++ Assets/Video/VideoPlayerScript.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class VideoPlayerScript : MonoBehaviour
@@ -8,53 +9,80 @@ public class VideoPlayerScript : MonoBehaviour
     [SerializeField]
     public List<VideoClip> videoClips;
     int clipNum = 0;
-    bool isStopped = false;
+    bool isStopped = true;
     private VideoPlayer videoPlayer;
-    // Start is called before the first frame update
+    private MeshRenderer meshRenderer;
+    [SerializeField]
+    Material play;
+    [SerializeField]
+    Material replay;
     void Start()
     {
         videoPlayer = GetComponentInChildren<VideoPlayer>();
+        if(videoClips.Count > 0)
         videoPlayer.clip = videoClips[0];
-        videoPlayer.Play();
+        meshRenderer = transform.Find("play").GetComponent<MeshRenderer>();
+        videoPlayer.Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isStopped && !isPlaying())
+        if (!isStopped && !isPlaying() && videoClips.Count > 0)
         {
-            Next();
-            videoPlayer.Play();
+            isStopped = true;
+            meshRenderer.material = replay;
+            meshRenderer.enabled = true;
         }
     }
 
     private void OnMouseDown()
     {
-        if (isStopped) videoPlayer.Play();
-        else videoPlayer.Pause();
-        isStopped = !isStopped;
+        if (videoClips.Count > 0)
+        {
+            if (isStopped)
+            {
+                if (!isPlaying())
+                {
+                    videoPlayer.Stop();
+                }
+                videoPlayer.Play();
+            }
+            else
+            {
+                meshRenderer.material = play;
+                videoPlayer.Pause();
+            }
+            isStopped = !isStopped;
+            meshRenderer.enabled = !meshRenderer.enabled;
+        }
     }
 
+    
     public void Back()
     {
-        if (clipNum == 0) clipNum = videoClips.Count;
-        clipNum--;
-        videoPlayer.clip = videoClips[clipNum];
-        isStopped = false;
+        if (videoClips.Count > 0)
+        {
+            if (clipNum == 0) clipNum = videoClips.Count;
+            clipNum--;
+            videoPlayer.clip = videoClips[clipNum];
+            meshRenderer.enabled = isStopped = false;
+        }
     }
 
     public void Next()
     {
-
-        clipNum++;
-        if (clipNum == videoClips.Count) clipNum = 0;
-        videoPlayer.clip = videoClips[clipNum];
-        isStopped = false;
+        if (videoClips.Count > 0)
+        {
+            clipNum++;
+            if (clipNum == videoClips.Count) clipNum = 0;
+            videoPlayer.clip = videoClips[clipNum];
+            meshRenderer.enabled = isStopped = false;
+        }
     }
 
     private bool isPlaying()
     {
-        Debug.Log(videoPlayer.frame + " " + (long)videoPlayer.frameCount);
         return (videoPlayer.frame+1) != (long)videoPlayer.frameCount;
     }
 }
